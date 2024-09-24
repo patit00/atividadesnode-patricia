c643d23f3932f4b084a4b93c50a4d67e9c2d9cd5
patit00 <129318253+patit00@users.noreply.github.com>
2024-09-24 23:05:01 +0000

    24042024

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const nomeCompleto ="Patricia Esmeralda" ;
+const prompt  = require('prompt-sync');
+const entrada = prompt();
+const nomeCompleto = "patricia esmeralda";
 const anoNascimento = 2007;
 let anoAtual = 2024;
-const idade = 17;
+const idade = 18;
+const A = "a";
+const E = "e";
+const O = "o";
+const Os = "os";
+const Para = "para";
+const Nao = "não";
 
-console.log("eu sou " + nomeCompleto + " estamos no ano" + anoAtual + " minha idade é " + idade + " e sou nascida no ano " + anoNascimento);
+console.log("Olá eu sou " + nomeCompleto + ", estamos no ano " + anoAtual + " e nasci no ano  " + anoNascimento + " " + E + " pretendo estar no exército, usarei " + Os + " benefícios que " + O + " exército possui para chegar aonde realmente quero. Pretendo começar tudo isso no ano de");
+
 anoAtual = anoAtual + 1;
+console.log(anoAtual);
 
-const proximoAno =" " ;
-anoAtual = anoAtual + 1;
-console.log ( " quero fazer faculdade de letras no próximo ano 2025 " + proximoAno);
+console.log(`crase ${nomeCompleto}, estamos no ano ${anoAtual} e nasci no ano ${anoNascimento} ${E} tamo ae cara, tenho ${idade} `);
 
-console.log(` oi eu sou ${nomeCompleto } meu ano de nascimento é ${anoNascimento} minha idade é ${idade}  `);  
+const ListaDeLocais = new Array(
+  `Negrozaque`,
+  `Niggeria`,
+  `IceLandQueNaoTemIce`,
+  `GreemLandQueNaoTemGreem`,
+  `Italiland`
+  );
 
-const listaDeViagens = new Array(
-  `Turquia`,
-  `Paris`,
- `Australia`,
-  `Mexico`,
-`Margarita`
-);
+console.log(ListaDeLocais);
 
-console.log(listaDeViagens);
+console.log(ListaDeLocais[4]);
+ListaDeLocais. push("CoronelDesvivido");
+console.log(ListaDeLocais);
 
-console.log(listaDeViagens[4])
-listaDeViagens.push ("Coronel Vivida");
-console.log(listaDeViagens);
+ListaDeLocais.splice(1,2);
+console.log(ListaDeLocais);
 
-listaDeViagens.splice(1,2);
-console.log(listaDeViagens);
+var nomeComprador = entrada("Qual teu nome? ")
+var idadeComprador = entrada("Qual sua idade? ");
 
-const idadeComprador= 17;
+if(idadeComprador <= 18){
 
-if(idadeComprador < 18){
-console.log ("iInfelizmente voce é menor de idade, não podemos le vender nossa passagens");
+  console.log(`ola, ${nomeComprador} sua voce não possuia a idade minima para fazer essa compra`)
 }
-if(idadeComprador >=18){
-  console.log("voce adquiriu a idade");
+if (idadeComprador >= 18){
+  console.log(`senhor(a) ${nomeComprador} seu dinheiro foi enviado com segunrança para o nosso bolso, agradeço sua compra`);
 }
